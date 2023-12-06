0732091e550b13c06b6d6c5ca19269e450104ee9
transon <transon1901@gmail.com>
2023-12-06 18:20:44 +0700

    tow

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,37 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+
+    /* */
+    'react-refresh/only-export-components': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@mui/*/*/*']
+      }
     ],
-  },
+    'no-console': 1,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    indent: ['warn', 2],
+    semi: [1, 'never'], // semi: [1, 'always'], cho phép sử dụng dấu chấm phẩy (;)
+    quotes: ['error', ''],
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0,
+    'no-unexpected-multiline': 'warn',
+    'keyword-spacing': 1,
+    'comma-dangle': 1, // kiểm tra xem có sử dụng dấu , cuối cùng trong object hoặc array hay không.
+    'comma-spacing': 1,
+    'arrow-spacing': 1
+  }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, FormGroup, FormControlLabel, Checkbox } from '@mui/material'
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleOk = () => {
+    return "hello"
+  }
   return (
     <>
-      <div>
-        Hello react JS
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variant="outlined">Hello world</Button>
+      <Button variant="outlined" color="success">Success</Button>
+      <Button variant="outlined" color="error">Error</Button>
+      <Button variant="outlined" startIcon={<AccessAlarm />}>Delete</Button>
+      <Button variant="outlined" startIcon={<ThreeDRotation />}>Send</Button>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+        <FormControlLabel required control={<Checkbox />} label="Required" />
+        <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+      </FormGroup>
     </>
   )
 }
